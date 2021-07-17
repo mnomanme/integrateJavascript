a737676bd474958c278516b460a0abbe7387f8de
nomanme <abdullah.noman083@gmail.com>
2021-07-17 12:29:14 +0600

    event bubble

---
+++ 18-function-add-event-listener-event-bubble/bubble/scripts.js
@@ -0,0 +1,11 @@
+document
+	.getElementById('first-item')
+	.addEventListener('click', function (event) {
+		console.log('first item clicked');
+	});
+
+document
+	.getElementById('container')
+	.addEventListener('click', function (event) {
+		console.log('container clicked');
+	});
