1b171bb23dbc9537f7363123ece9602a03ff1bdf
nomanme <abdullah.noman083@gmail.com>
2021-07-17 12:46:17 +0600

    stop event

--- 18-function-add-event-listener-event-bubble/bubble/scripts.js
+++ 18-function-add-event-listener-event-bubble/bubble/scripts.js
@@ -2,7 +2,13 @@ document
 	.getElementById('first-item')
 	.addEventListener('click', function (event) {
 		console.log('first item clicked');
+		event.stopPropagation();
 	});
+
+document.getElementById('my-list').addEventListener('click', function (event) {
+	console.log('my list clicked');
+	// event.stopImmediatePropagation();
+});
 
 document
 	.getElementById('container')
