5997565a97e10d783df7642af7440dba152aae44
nomanme <abdullah.noman083@gmail.com>
2021-07-17 11:13:43 +0600

    add event listener

--- 18-function-add-event-listener-event-bubble/events/scripts.js
+++ 18-function-add-event-listener-event-bubble/events/scripts.js
@@ -1,3 +1,20 @@
 function handleClicked() {
 	console.log('Someone Clicked Me');
+	document.body.style.backgroundColor = 'lightgreen';
 }
+
+// second button
+const secondButton = document.getElementById('second');
+secondButton.onclick = function secondClick() {
+	console.log('Second person press me');
+};
+
+// third button
+const thirdButton = document.getElementById('third');
+thirdButton.addEventListener('click', handleClicked);
+thirdButton.onclick = handleClicked;
+
+// first button
+document.getElementById('first').addEventListener('click', function () {
+	document.body.style.color = 'crimson';
+});
