2df06d0f533637af50b58ee205484ca9a1ae93f8
nomanme <abdullah.noman083@gmail.com>
2021-07-17 13:27:55 +0600

    event delegate

--- 18-function-add-event-listener-event-bubble/bubble/scripts.js
+++ 18-function-add-event-listener-event-bubble/bubble/scripts.js
@@ -1,17 +1,46 @@
+// li first item
 document
 	.getElementById('first-item')
 	.addEventListener('click', function (event) {
-		console.log('first item clicked');
+		// console.log('first item clicked');
 		event.stopPropagation();
 	});
 
+// ul my list
 document.getElementById('my-list').addEventListener('click', function (event) {
-	console.log('my list clicked');
-	// event.stopImmediatePropagation();
+	// console.log('my list clicked');
+	event.stopImmediatePropagation();
 });
 
+// container
 document
 	.getElementById('container')
 	.addEventListener('click', function (event) {
-		console.log('container clicked');
+		// console.log('container clicked');
 	});
+
+// button
+var item = document.getElementsByClassName('item');
+
+for (let i = 0; i < item.length; i++) {
+	const items = item[i];
+	// console.log(items);
+	items.addEventListener('click', function (event) {
+		event.target.parentNode.removeChild(event.target);
+		// console.log(this);
+		// console.log(event.target.innerText);
+		// console.log(event.target.parentNode);
+	});
+}
+
+document.getElementById('add-new').addEventListener('click', function (event) {
+	var itemParent = document.getElementById('my-list');
+	var newItem = document.createElement('li');
+	newItem.innerHTML = 'brand new item';
+	itemParent.appendChild(newItem);
+});
+
+document.getElementById('my-list').addEventListener('click', function (event) {
+	// console.log(this, event.target);
+	event.target.parentNode.removeChild(event.target);
+});
