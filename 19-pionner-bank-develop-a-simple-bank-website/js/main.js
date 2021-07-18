ac9e6aa3e6f14f23955c021d690f15f487b7c2ef
nomanme <abdullah.noman083@gmail.com>
2021-07-19 01:22:41 +0600

    deposit button event handler

--- 19-pionner-bank-develop-a-simple-bank-website/js/main.js
+++ 19-pionner-bank-develop-a-simple-bank-website/js/main.js
@@ -1,5 +1,4 @@
-// login button
-
+// login button event handler
 const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function () {
 	// console.log('login button works now');
@@ -7,4 +6,13 @@ loginBtn.addEventListener('click', function () {
 	loginArea.style.display = 'none';
 	const transactionArea = document.getElementById('transaction-area');
 	transactionArea.style.display = 'block';
+});
+
+// deposit button event handler
+const deposit = document.getElementById('addDeposit');
+deposit.addEventListener('click', function () {
+	// console.log('deposit clicked');
+	const depositAmount = document.getElementById('depositAmount').value;
+	const depositNumber = parseFloat(depositAmount);
+	console.log(depositNumber);
 });
