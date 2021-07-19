05274bfd1bf8051b5ff7edd6c33097e046be082b
nomanme <abdullah.noman083@gmail.com>
2021-07-19 10:47:42 +0600

    update two fields same time

--- 19-pionner-bank-develop-a-simple-bank-website/js/main.js
+++ 19-pionner-bank-develop-a-simple-bank-website/js/main.js
@@ -13,14 +13,31 @@ const deposit = document.getElementById('addDeposit');
 deposit.addEventListener('click', function () {
 	// console.log('deposit clicked');
 	const depositAmount = document.getElementById('depositAmount').value;
-	const depositNumber = parseFloat(depositAmount);
+	const amountNumber = parseFloat(depositAmount);
 	// console.log(depositNumber);
 
+	/*
 	const currentDeposit = document.getElementById('currentDeposit').innerText;
 	const currentDepositNumber = parseFloat(currentDeposit);
-
 	const totalDeposit = currentDepositNumber + depositNumber;
-
 	document.getElementById('currentDeposit').innerText = totalDeposit;
+
+	
+	const currentBalance = document.getElementById('currentBalance').innerText;
+	const currentBalanceNumber = parseFloat(currentBalance);
+	const totalBalance = depositNumber + currentBalanceNumber;
+	document.getElementById('currentBalance').innerText = totalBalance;
+	*/
+
+	updateSpanText('currentDeposit', amountNumber);
+	updateSpanText('currentBalance', amountNumber);
+
 	document.getElementById('depositAmount').value = '';
 });
+
+function updateSpanText(id, amountNumber) {
+	const currentAmount = document.getElementById(id).innerText;
+	const currentAmountNumber = parseFloat(currentAmount);
+	const totalAmount = amountNumber + currentAmountNumber;
+	document.getElementById(id).innerText = totalAmount;
+}
