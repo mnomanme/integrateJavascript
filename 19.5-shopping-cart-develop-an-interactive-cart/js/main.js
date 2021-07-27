acc7c492dcebe516268b8c4867f6da90ef62e9da
nomanme <abdullah.noman083@gmail.com>
2021-07-27 23:46:29 +0600

    case increase also total

--- 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
+++ 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
@@ -1,1 +1,11 @@
-console.log(21);
+document.getElementById('case-increament').addEventListener('click', function () {
+	// console.log('case increase');
+	const caseInput = document.getElementById('case-count');
+	const caseCount = parseInt(caseInput.value);
+	// console.log(caseCount);
+	const caseNewCount = caseCount + 1;
+	caseInput.value = caseNewCount;
+
+	const caseTotal = caseNewCount * 60;
+	document.getElementById('case-price').innerText = caseTotal;
+});
