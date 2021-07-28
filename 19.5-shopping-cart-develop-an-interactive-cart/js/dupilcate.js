7f59d7a659385928e0d29772c70b395dec287018
nomanme <abdullah.noman083@gmail.com>
2021-07-29 01:14:27 +0600

    duplicate js

--- 19.5-shopping-cart-develop-an-interactive-cart/js/dupilcate.js
+++ 19.5-shopping-cart-develop-an-interactive-cart/js/dupilcate.js
@@ -1,4 +1,30 @@
 // second solution
+// phone increament
+document.getElementById('phone-increament').addEventListener('click', function () {
+	handlePhoneChange(true);
+});
+
+// phone decreament
+document.getElementById('phone-decreament').addEventListener('click', function () {
+	handlePhoneChange(false);
+});
+function handlePhoneChange(isIncrease) {
+	const phoneInput = document.getElementById('phone-count');
+	const phoneCount = parseInt(phoneInput.value);
+
+	let phoneNewCount = phoneCount;
+	if (isIncrease === true) {
+		phoneNewCount = phoneCount + 1;
+	}
+	if (isIncrease === false && phoneNewCount > 0) {
+		phoneNewCount = phoneCount - 1;
+	}
+
+	phoneInput.value = phoneNewCount;
+	const phoneTotal = phoneNewCount * 1200;
+	document.getElementById('phone-price').innerText = phoneTotal;
+}
+
 // case increament
 document.getElementById('case-increament').addEventListener('click', function () {
 	handleProductChange(true);
@@ -28,6 +54,34 @@ function handleProductChange(isIncrease) {
 }
 
 // first solution
+// phone increament
+document.getElementById('phone-increament').addEventListener('click', function () {
+	// console.log('phone increase');
+	const phoneInput = document.getElementById('phone-count');
+	// console.log(typeof phoneInput.value);
+	const phoneCount = parseInt(phoneInput.value);
+	// console.log(phoneCount);
+	const phoneNewCount = phoneCount + 1;
+	phoneInput.value = phoneNewCount;
+
+	const phoneTotal = phoneNewCount * 1200;
+	document.getElementById('phone-price').innerText = phoneTotal;
+});
+
+// phone decreament
+document.getElementById('phone-decreament').addEventListener('click', function () {
+	// console.log('phone decrease');
+	const phoneInput = document.getElementById('phone-count');
+	// console.log(typeof phoneInput.value);
+	const phoneCount = parseInt(phoneInput.value);
+	// console.log(typeof phoneCount);
+	const phoneNewCount = phoneCount - 1;
+	phoneInput.value = phoneNewCount;
+
+	const phoneTotal = phoneNewCount * 1200;
+	document.getElementById('phone-price').innerText = phoneTotal;
+});
+
 // case increament
 document.getElementById('case-increament').addEventListener('click', function () {
 	// console.log('case increase');
