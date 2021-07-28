19f4bbea07cec2950bc4df3a69a1d0ca2c2e2868
nomanme <abdullah.noman083@gmail.com>
2021-07-29 01:29:05 +0600

    duplicate add

--- 19.5-shopping-cart-develop-an-interactive-cart/js/dupilcate.js
+++ 19.5-shopping-cart-develop-an-interactive-cart/js/dupilcate.js
@@ -1,3 +1,42 @@
+// third solution
+// case increase & decrease
+function handleCaseChange(isIncrease) {
+	const caseOutput = document.getElementById('case-count');
+	const caseCount = parseInt(caseOutput.value);
+
+	let caseNewCount = caseCount;
+
+	if (isIncrease === true) {
+		caseNewCount = caseCount + 1;
+	}
+	if (isIncrease === false && caseNewCount > 0) {
+		caseNewCount = caseCount - 1;
+	}
+
+	caseOutput.value = caseNewCount;
+	const caseTotal = caseNewCount * 60;
+	document.getElementById('case-price').innerText = caseTotal;
+}
+
+// phone increament & decreament
+
+function handlePhoneChange(isIncrease) {
+	const phoneInput = document.getElementById('phone-count');
+	const phoneCount = parseInt(phoneInput.value);
+
+	let phoneNewCount = phoneCount;
+	if (isIncrease === true) {
+		phoneNewCount = phoneCount + 1;
+	}
+	if (isIncrease === false && phoneNewCount > 0) {
+		phoneNewCount = phoneCount - 1;
+	}
+
+	phoneInput.value = phoneNewCount;
+	const phoneTotal = phoneNewCount * 1200;
+	document.getElementById('phone-price').innerText = phoneTotal;
+}
+
 // second solution
 // phone increament
 document.getElementById('phone-increament').addEventListener('click', function () {
@@ -27,15 +66,16 @@ function handlePhoneChange(isIncrease) {
 
 // case increament
 document.getElementById('case-increament').addEventListener('click', function () {
-	handleProductChange(true);
+	handleCaseChange(true);
 });
 
 // case decreament
 document.getElementById('case-decreament').addEventListener('click', function () {
-	handleProductChange(false);
+	handleCaseChange(false);
 });
 
-function handleProductChange(isIncrease) {
+//function
+function handleCaseChange(isIncrease) {
 	const caseOutput = document.getElementById('case-count');
 	const caseCount = parseInt(caseOutput.value);
 
