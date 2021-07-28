6d737baa8e18ef6a0b50d093d98f33f4adb8ca29
nomanme <abdullah.noman083@gmail.com>
2021-07-29 01:28:38 +0600

    product duplicate remove

--- 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
+++ 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
@@ -1,36 +1,17 @@
-// case increase & decrease
-function handleCaseChange(isIncrease) {
-	const caseOutput = document.getElementById('case-count');
-	const caseCount = parseInt(caseOutput.value);
+function handleProductChange(productAdd) {
+	const productInput = document.getElementById('case-count');
+	const productCount = parseInt(productInput.value);
 
-	let caseNewCount = caseCount;
+	let productNewCount = productCount;
 
-	if (isIncrease === true) {
-		caseNewCount = caseCount + 1;
+	if (productAdd === true) {
+		productNewCount = productCount + 1;
 	}
-	if (isIncrease === false && caseNewCount > 0) {
-		caseNewCount = caseCount - 1;
-	}
-
-	caseOutput.value = caseNewCount;
-	const caseTotal = caseNewCount * 60;
-	document.getElementById('case-price').innerText = caseTotal;
-}
-// phone increament & decreament
-
-function handlePhoneChange(isIncrease) {
-	const phoneInput = document.getElementById('phone-count');
-	const phoneCount = parseInt(phoneInput.value);
-
-	let phoneNewCount = phoneCount;
-	if (isIncrease === true) {
-		phoneNewCount = phoneCount + 1;
-	}
-	if (isIncrease === false && phoneNewCount > 0) {
-		phoneNewCount = phoneCount - 1;
+	if (productAdd === false && productNewCount > 0) {
+		productNewCount = productCount - 1;
 	}
 
-	phoneInput.value = phoneNewCount;
-	const phoneTotal = phoneNewCount * 1200;
-	document.getElementById('phone-price').innerText = phoneTotal;
+	productInput.value = productNewCount;
+	const productTotal = productNewCount * 60;
+	document.getElementById('case-price').innerText = productTotal;
 }
