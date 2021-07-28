bc95b23ba523181e34ee6c8fe299fe38f68a7d7f
nomanme <abdullah.noman083@gmail.com>
2021-07-28 20:09:56 +0600

    phone addEventListener add

--- 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
+++ 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
@@ -15,3 +15,31 @@ function handleProductChange(isIncrease) {
 	const caseTotal = caseNewCount * 60;
 	document.getElementById('case-price').innerText = caseTotal;
 }
+
+// phone increament
+document.getElementById('phone-increament').addEventListener('click', function () {
+	// console.log('phone increase');
+	const phoneInput = document.getElementById('phone-count');
+	// console.log(typeof phoneInput.value);
+	const phoneCount = parseInt(phoneInput.value);
+	// console.log(phoneCount);
+	const phoneNewCount = phoneCount + 1;
+	phoneInput.value = phoneNewCount;
+
+	const phoneTotal = phoneNewCount * 1200;
+	document.getElementById('phone-price').innerText = phoneTotal;
+});
+
+// phone decreament
+document.getElementById('phone-decreament').addEventListener('click', function () {
+	// console.log('phone decrease');
+	const phoneInput = document.getElementById('phone-count');
+	// console.log(typeof phoneInput.value);
+	const phoneCount = parseInt(phoneInput.value);
+	// console.log(typeof phoneCount);
+	const phoneNewCount = phoneCount - 1;
+	phoneInput.value = phoneNewCount;
+
+	const phoneTotal = phoneNewCount * 1200;
+	document.getElementById('phone-price').innerText = phoneTotal;
+});
