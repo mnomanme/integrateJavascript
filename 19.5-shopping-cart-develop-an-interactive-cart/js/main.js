a740a25137e199325b89bc3a65eff1c78b69a325
nomanme <abdullah.noman083@gmail.com>
2021-07-29 10:22:37 +0600

    calculate all product

--- 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
+++ 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
@@ -1,17 +1,22 @@
-function handleProductChange(productAdd) {
-	const productInput = document.getElementById('case-count');
+function handleProductChange(product, productAdd) {
+	const productInput = document.getElementById(product + '-count');
 	const productCount = parseInt(productInput.value);
 
 	let productNewCount = productCount;
-
 	if (productAdd === true) {
 		productNewCount = productCount + 1;
 	}
 	if (productAdd === false && productNewCount > 0) {
 		productNewCount = productCount - 1;
 	}
-
 	productInput.value = productNewCount;
-	const productTotal = productNewCount * 60;
-	document.getElementById('case-price').innerText = productTotal;
+
+	let productTotal = 0;
+	if (product === 'phone') {
+		productTotal = productNewCount * 1200;
+	}
+	if (product === 'case') {
+		productTotal = productNewCount * 60;
+	}
+	document.getElementById(product + '-price').innerText = productTotal;
 }
