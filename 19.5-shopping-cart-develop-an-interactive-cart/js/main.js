702a0ebb20ec00399a1adc50a6d3f2e8eeeda3a2
nomanme <abdullah.noman083@gmail.com>
2021-07-29 11:17:39 +0600

    return

--- 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
+++ 19.5-shopping-cart-develop-an-interactive-cart/js/main.js
@@ -1,6 +1,7 @@
+// product calculate
 function handleProductChange(product, productAdd) {
 	const productInput = document.getElementById(product + '-count');
-	const productCount = parseInt(productInput.value);
+	const productCount = getInputValue(product);
 
 	let productNewCount = productCount;
 	if (productAdd === true) {
@@ -19,4 +20,28 @@ function handleProductChange(product, productAdd) {
 		productTotal = productNewCount * 60;
 	}
 	document.getElementById(product + '-price').innerText = productTotal;
+	// subtotal
+	calculateTotal();
+}
+
+// calculate subtotal
+function calculateTotal() {
+	const phoneCount = getInputValue('phone');
+	const caseCount = getInputValue('case');
+	const subtotal = phoneCount * 1200 + caseCount * 60;
+	document.getElementById('sub-total').innerText = subtotal;
+
+	const tax = subtotal / 10;
+	document.getElementById('tax').innerText = tax;
+
+	const total = subtotal + tax;
+	document.getElementById('total').innerText = total;
+}
+
+// calculate increament
+function getInputValue(product) {
+	const productInput = document.getElementById(product + '-count');
+	const productCount = parseInt(productInput.value);
+
+	return productCount;
 }
