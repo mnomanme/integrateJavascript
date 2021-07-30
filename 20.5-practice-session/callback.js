87e894e85279524511f90dbc6ffabca118a63a01
nomanme <abdullah.noman083@gmail.com>
2021-07-31 01:49:18 +0600

    callback working

--- 20.5-practice-session/callback.js
+++ 20.5-practice-session/callback.js
@@ -1,16 +1,36 @@
 // 21_5-4 When and how to use javascript call back function
-function welcomeGuest(name, getHandler) {
-	// console.log(name);
-	getHandler(name);
+function welComeGuest(name, wishHandler) {
+	// console.log('Guest Name =>', name);
+	// console.log('wish handler =>', wishHandler);
+	// console.log('wish handler name =>', name);
+	wishHandler(name);
 }
+
 const actorName = 'Tom Hanks';
+// welComeGuest(actorName);
 
-function greetMoring(name) {
-	console.log('Good Morning', name);
-}
-function greetAfternoon(name) {
-	console.log('Good After Noon', name);
+function greetMorning(wish) {
+	console.log('Good Morning', wish);
 }
 
-welcomeGuest(actorName, greetMoring);
-welcomeGuest('Tamim Iqbal', greetAfternoon);
+function greetEvening(wish2) {
+	console.log('Good Evening', wish2);
+}
+
+function greetNight(wish3) {
+	console.log('Good Night', wish3);
+}
+
+welComeGuest(actorName, greetMorning);
+welComeGuest('Tamim Iqbal', greetEvening);
+welComeGuest('Shakib Khan', greetNight);
+
+welComeGuest('Chirs Gayle', function (player) {
+	console.log('Special Welcome', player);
+});
+
+function handleClicked() {
+	console.log('someone clicked me');
+}
+
+// document.getElementById('click').addEventListener('click', handleClicked);
