fbeedf36f82efca197e51b2e32e1df89e9f2321d
nomanme <abdullah.noman083@gmail.com>
2021-07-31 23:54:58 +0600

    duplicate code

---
+++ 21-assignment/cruise-queen/js/duplicate.js
@@ -0,0 +1,28 @@
+// first class ticket increase
+
+document.getElementById('firstClassIncreament').addEventListener('click', function () {
+	// console.log('plus increase');
+	const firstClassInput = document.getElementById('firstClassCount');
+	const firstClassCount = parseInt(firstClassInput.value);
+	// console.log(typeof firstClassCount);
+	const firstClassNewCount = firstClassCount + 1;
+	// console.log(firstClassNewCount);
+	firstClassInput.value = firstClassNewCount;
+	// subtotal
+	const firstClassTicketPrice = firstClassNewCount * 150;
+	document.getElementById('subTotal').innerText = firstClassTicketPrice;
+});
+
+// first class ticket decrease
+
+document.getElementById('firstClassDecreament').addEventListener('click', function () {
+	// console.log('minus decrease');
+	const firstClassInput = document.getElementById('firstClassCount');
+	const firstClassCount = parseInt(firstClassInput.value);
+	const firstClassNewCount = firstClassCount - 1;
+	// console.log(firstClassNewCount);
+	firstClassInput.value = firstClassNewCount;
+	// subtotal
+	const firstClassTicketPrice = firstClassNewCount * 150;
+	document.getElementById('subTotal').innerText = firstClassTicketPrice;
+});
