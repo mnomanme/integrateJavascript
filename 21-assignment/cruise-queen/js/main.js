65a257dc907d191381c6700d0583ac0d16416f19
nomanme <abdullah.noman083@gmail.com>
2021-07-31 23:54:00 +0600

    function handler

--- 21-assignment/cruise-queen/js/main.js
+++ 21-assignment/cruise-queen/js/main.js
@@ -1,26 +1,26 @@
 // first class ticket increase
 document.getElementById('firstClassIncreament').addEventListener('click', function () {
-	// console.log('plus increase');
-	const firstClassInput = document.getElementById('firstClassCount');
-	const firstClassCount = parseInt(firstClassInput.value);
-	// console.log(typeof firstClassCount);
-	const firstClassNewCount = firstClassCount + 1;
-	// console.log(firstClassNewCount);
-	firstClassInput.value = firstClassNewCount;
-	// subtotal
-	const firstClassTicketPrice = firstClassNewCount * 150;
-	document.getElementById('subTotal').innerText = firstClassTicketPrice;
+	handleFirstClassTicket(true);
 });
 
 // first class ticket decrease
 document.getElementById('firstClassDecreament').addEventListener('click', function () {
-	// console.log('minus decrease');
-	const firstClassInput = document.getElementById('firstClassCount');
-	const firstClassCount = parseInt(firstClassInput.value);
-	const firstClassNewCount = firstClassCount - 1;
-	// console.log(firstClassNewCount);
-	firstClassInput.value = firstClassNewCount;
-	// subtotal
-	const firstClassTicketPrice = firstClassNewCount * 150;
-	document.getElementById('subTotal').innerText = firstClassTicketPrice;
+	handleFirstClassTicket(false);
 });
+
+function handleFirstClassTicket(addFirstTicket) {
+	const firstClassTicket = document.getElementById('firstClassCount');
+	const firstClassTicketCount = parseInt(firstClassTicket.value);
+
+	let firstClassNewTicket = firstClassTicketCount;
+	if (addFirstTicket == true) {
+		firstClassNewTicket = firstClassTicketCount + 1;
+	}
+	if (addFirstTicket === false && firstClassNewTicket > 0) {
+		firstClassNewTicket = firstClassTicketCount - 1;
+	}
+
+	firstClassTicket.value = firstClassNewTicket;
+	const firstClassTicketPrice = firstClassNewTicket * 150;
+	document.getElementById('subTotal').innerText = firstClassTicketPrice;
+}
