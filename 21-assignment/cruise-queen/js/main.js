8b1cc5077d6c6dba87f1517aa0cc730dbea3ab86
nomanme <abdullah.noman083@gmail.com>
2021-08-01 14:45:06 +0600

    handle economy class ticket

--- 21-assignment/cruise-queen/js/main.js
+++ 21-assignment/cruise-queen/js/main.js
@@ -1,4 +1,4 @@
-// first class ticket increase
+// first class ticket
 function handleFirstClassTicket(addFirstTicket) {
 	const firstClassTicket = document.getElementById('firstClassCount');
 	const firstClassTicketCount = parseInt(firstClassTicket.value);
@@ -11,12 +11,37 @@ function handleFirstClassTicket(addFirstTicket) {
 		firstClassNewTicket = firstClassTicketCount - 1;
 	}
 	firstClassTicket.value = firstClassNewTicket;
+	// calculate first class ticket price
+	// const firstClassTicketPrice = firstClassNewTicket * 150;
+	// const firstClassTicketVat = firstClassTicketPrice / 10;
+	// const firstClassTicketTotal = firstClassTicketPrice + firstClassTicketVat;
 
-	const firstClassTicketPrice = firstClassNewTicket * 150;
-	const firstClassTicketVat = firstClassTicketPrice / 10;
-	const firstClassTicketTotal = firstClassTicketPrice + firstClassTicketVat;
+	// document.getElementById('subTotal').innerText = firstClassTicketPrice;
+	// document.getElementById('vatCharge').innerText = firstClassTicketVat;
+	// document.getElementById('total').innerText = firstClassTicketTotal;
+	ticketTotal();
+}
 
-	document.getElementById('subTotal').innerText = firstClassTicketPrice;
-	document.getElementById('vatCharge').innerText = firstClassTicketVat;
-	document.getElementById('total').innerText = firstClassTicketTotal;
+// economy class ticket
+function handleEconomyTicket(addEconomyTicket) {
+	const economyTicket = document.getElementById('economyCount');
+	const economyTicketCount = parseInt(economyTicket.value);
+
+	let economyNewTicket = economyTicketCount;
+	if (addEconomyTicket === true) {
+		economyNewTicket = economyTicketCount + 1;
+	}
+	if (addEconomyTicket === false && economyNewTicket > 0) {
+		economyNewTicket = economyTicketCount - 1;
+	}
+	economyTicket.value = economyNewTicket;
+	// calculate economy ticket price
+	// const economyTicketPrice = economyNewTicket * 100;
+	// const economyTicketVat = economyTicketPrice / 10;
+	// const economyTicketTotal = economyTicketPrice + economyTicketVat;
+
+	// document.getElementById('subTotal').innerText = economyTicketPrice;
+	// document.getElementById('vatCharge').innerText = economyTicketVat;
+	// document.getElementById('total').innerText = economyTicketTotal;
+	ticketTotal();
 }
