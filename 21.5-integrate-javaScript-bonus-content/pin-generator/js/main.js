4373e75c22f8da670ac54b648ee5fb4c8a0b516b
nomanme <abdullah.noman083@gmail.com>
2021-08-02 00:41:32 +0600

    digit show

--- 21.5-integrate-javaScript-bonus-content/pin-generator/js/main.js
+++ 21.5-integrate-javaScript-bonus-content/pin-generator/js/main.js
@@ -2,6 +2,7 @@
 document.getElementById('successSign').style.display = 'none';
 document.getElementById('dangerSign').style.display = 'none';
 
+// generate random pin number
 function handleRandomPin() {
 	const randomPin = Math.random() * 10000;
 	const pinCount = (randomPin + '').split('.')[0];
@@ -13,8 +14,23 @@ function handleRandomPin() {
 	}
 }
 
-// generate pin
+// generate pin button
 function generatePin() {
 	const generatePinBtn = document.getElementById('pinInput');
 	generatePinBtn.value = handleRandomPin();
 }
+
+// submit digit button
+const buttonInput = document.getElementById('buttonDigit').addEventListener('click', function (event) {
+	// console.log(event.target.innerText);
+	const btnNumber = event.target.innerText;
+
+	if (isNaN(btnNumber)) {
+		// handle backspace
+		// handle clear
+		console.log('not a number');
+	} else {
+		const typeInput = document.getElementById('digitShow');
+		typeInput.value = typeInput.value + btnNumber;
+	}
+});
