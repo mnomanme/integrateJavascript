bf3f9d5125366e1ff73913e9957153b639746850
nomanme <abdullah.noman083@gmail.com>
2021-08-02 01:14:52 +0600

    match result

--- 21.5-integrate-javaScript-bonus-content/pin-generator/js/main.js
+++ 21.5-integrate-javaScript-bonus-content/pin-generator/js/main.js
@@ -20,17 +20,45 @@ function generatePin() {
 	generatePinBtn.value = handleRandomPin();
 }
 
-// submit digit button
+// submit field input digit button
 const buttonInput = document.getElementById('buttonDigit').addEventListener('click', function (event) {
 	// console.log(event.target.innerText);
 	const btnNumber = event.target.innerText;
 
 	if (isNaN(btnNumber)) {
-		// handle backspace
 		// handle clear
+		if (btnNumber === 'C') {
+			const typeInput = document.getElementById('digitShow');
+			typeInput.value = '';
+		}
+		// handle backspace
+		// if (btnNumber === 'B') {
+		// 	const typeInput = document.getElementById('digitShow');
+		// 	typeInput.value = btnNumber - typeInput.value;
+		// }
 		console.log('not a number');
 	} else {
 		const typeInput = document.getElementById('digitShow');
 		typeInput.value = typeInput.value + btnNumber;
 	}
 });
+
+// submit button matching
+const submitVerify = document.getElementById('submit').addEventListener('click', function () {
+	const generatePin = document.getElementById('pinInput').value;
+	const typePin = document.getElementById('digitShow').value;
+
+	if (generatePin === typePin) {
+		matchingResult('block', 'none');
+	} else {
+		matchingResult('none', 'block');
+	}
+});
+
+// matching result
+function matchingResult(correctSign, incorrectSign) {
+	const success = document.getElementById('successSign');
+	success.style.display = correctSign;
+	const incorrect = document.getElementById('dangerSign');
+	incorrect.style.display = incorrectSign;
+}
