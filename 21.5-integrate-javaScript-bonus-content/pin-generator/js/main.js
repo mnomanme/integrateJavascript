88e515e5e6ff59293ab622709c43223d04a8d5e8
nomanme <abdullah.noman083@gmail.com>
2021-08-02 00:16:24 +0600

    generate pin add

--- 21.5-integrate-javaScript-bonus-content/pin-generator/js/main.js
+++ 21.5-integrate-javaScript-bonus-content/pin-generator/js/main.js
@@ -1,3 +1,20 @@
 // success & danger sign
 document.getElementById('successSign').style.display = 'none';
 document.getElementById('dangerSign').style.display = 'none';
+
+function handleRandomPin() {
+	const randomPin = Math.random() * 10000;
+	const pinCount = (randomPin + '').split('.')[0];
+
+	if (pinCount.length === 4) {
+		return pinCount;
+	} else {
+		return handleRandomPin();
+	}
+}
+
+// generate pin
+function generatePin() {
+	const generatePinBtn = document.getElementById('pinInput');
+	generatePinBtn.value = handleRandomPin();
+}
