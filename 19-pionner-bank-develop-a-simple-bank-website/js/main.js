f1bc970a2c80f17f07237afa5cb52e4d3d241e65
nomanme <abdullah.noman083@gmail.com>
2021-08-06 18:22:36 +0600

    update

--- 19-pionner-bank-develop-a-simple-bank-website/js/main.js
+++ 19-pionner-bank-develop-a-simple-bank-website/js/main.js
@@ -3,9 +3,17 @@ const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function () {
 	// console.log('login button works now');
 	const loginArea = document.getElementById('login-area');
-	loginArea.style.display = 'none';
 	const transactionArea = document.getElementById('transaction-area');
-	transactionArea.style.display = 'block';
+	const name = document.getElementById('name');
+	const email = document.getElementById('email');
+	const password = document.getElementById('password');
+
+	if ((email.value !== '') & (password.value !== '') && name.value !== '') {
+		loginArea.style.display = 'none';
+		transactionArea.style.display = 'block';
+	} else {
+		alert('Please Insert Your Email & Password');
+	}
 });
 
 // deposit button event handler
